e754927eb87a67e0485c280b20df47386073b7cd
labrac <raquelortegalabrac@hotmail.com>
2023-05-04 01:59:02 +0200

    Remove comments

--- app/utils.js
+++ app/utils.js
@@ -1,5 +1,3 @@
-// Export const strictEquals = (valueA, valueB) => Object.is(valueA, valueB);
-
 import counter from "./index.js";
 
 counter.addOne();
--- app/utils.test.js
+++ app/utils.test.js
@@ -3,20 +3,6 @@
  */
 import { addCustomOne } from "./index.js";
 
-// Describe("Given a strictEquals function", () => {
-//   describe("When it receives 1 and 1", () => {
-//     test("Then it should returns true", () => {
-//       const valueA = 1;
-//       const valueB = 1;
-// const expectedResult = strictEquals(valueA, valueB);
-//       const result = true;
-//
-
-//       expect(expectedResult).toBe(result);
-//     });
-//   });
-// });
-
 describe("Given addCustomOne function", () => {
   describe("When it receives 5", () => {
     test("Then it should returns 6", () => {
